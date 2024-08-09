246596daf8f29e5e958561de6deef36fbe935c3c
Ami Schreiber <ami.schreiber@live.com>
2024-08-09 04:49:53 +0000

    removed content parameter

--- app/SharedWebComponents/Services/IPdfViewer.cs
+++ app/SharedWebComponents/Services/IPdfViewer.cs
@@ -4,5 +4,5 @@ namespace SharedWebComponents.Services;
 
 public interface IPdfViewer
 {
-    ValueTask ShowDocumentAsync(string name, string baseUrl, string content);
+    ValueTask ShowDocumentAsync(string name, string baseUrl);
 }
