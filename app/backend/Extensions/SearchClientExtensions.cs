bd278d6d0098925f3f3dcf31698851e783edfbe3
Ami Schreiber <ami.schreiber@live.com>
2024-08-09 06:16:13 +0000

    fixed baseUrl references

--- app/backend/Extensions/SearchClientExtensions.cs
+++ app/backend/Extensions/SearchClientExtensions.cs
@@ -85,12 +85,12 @@ internal static class SearchClientExtensions
                 if (useSemanticCaptions)
                 {
                     var docs = doc.SemanticSearch.Captions.Select(c => c.Text);
-                    baseUrlValue = string.Join(baseUrl,sourcePageValue);
+                    baseUrlValue = string.Join("/",sourcePageValue);
                     contentValue = string.Join(" . ", docs);
                 }
                 else
                 {                
-                    baseUrlValue = string.Join(baseUrl,sourcePageValue);
+                    baseUrlValue = string.Join("/",sourcePageValue);
                     doc.Document.TryGetValue("content", out var value);
                     contentValue = (string)value;
                 }
@@ -104,7 +104,7 @@ internal static class SearchClientExtensions
             if (sourcePageValue is string sourcePage && baseUrlValue is string baseUrl && contentValue is string content)
             {
                 content = content.Replace('\r', ' ').Replace('\n', ' ');
-                baseUrl = string.Join(baseUrl,sourcePage);
+                baseUrl = string.Join("/",sourcePage);
                 sb.Add(new SupportingContentRecord(sourcePage,baseUrl,content));
             }
         }
